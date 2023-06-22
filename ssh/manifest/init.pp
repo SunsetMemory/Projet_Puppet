67f4a66230fe4b5c8d4baa0a42ef2d85ce4dd573
SunsetMemory <81764792+SunsetMemory@users.noreply.github.com>
2023-06-22 11:54:00 +0200

    delete ssh 'manifest' folder

--- ssh/manifest/init.pp
+++
@@ -1,8 +0,0 @@
-# This class manages the dependencies of other classes
-
-class ssh {
-  include ssh::params
-  include ssh::install
-  include ssh::config
-  include ssh::service
-}
--- ssh/manifest/install.pp
+++
@@ -1,7 +0,0 @@
-# This class installs the required packages
-
-class ssh::install {
-  package { $ssh::params::package_name:
-    ensure => installed,
-  }
-}
